198e86166dadbb27944805417c7b89d255280b67
Fausto Rendon <faustor001@gmail.com>
2019-01-14 21:24:56 -0800

    Add styling to images

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
+import config from './config';
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization:
-      'Client-ID 2fdf813f58b790c9c48c8ea7a470257cad477334246a1a4a2f242d15879a416e'
+    Authorization: 'Client-ID ' + config.key
   }
 });
