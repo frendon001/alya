2ebc751dc0146955da759d0a6c839a4df8843bd8
Fausto Rendon <faustor001@gmail.com>
2019-02-07 19:12:18 -0800

    Refactor folder structure and adding pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,10 @@
 import React from 'react';
-import SearchBar from './SearchBar';
-import ImageList from './ImageList';
-import { connect } from 'react-redux';
-import { fetchPhotos } from '../actions';
-import { bindActionCreators } from 'redux';
+import SearchPhotosPage from './pages/SearchPhotosPage';
 
 class App extends React.Component {
-  state = { images: [] };
-
-  onSearchSubmit = term => {
-    this.props.fetchPhotos(term, 1);
-  };
-
   render() {
-    const photos = this.props.photos;
-    return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {photos.length} images
-        <ImageList images={photos} />
-      </div>
-    );
+    return <SearchPhotosPage />;
   }
 }
 
-function mapStateToProps({ photos }) {
-  return { photos };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchPhotos }, dispatch);
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;
